dca6f0a34cd2464b616301d1b83d848f82af2d0a
ArtemRysich <110498892+ArtemRysich@users.noreply.github.com>
2023-04-22 21:04:50 +0300

    info item

--- src/realty.js
+++ src/realty.js
@@ -1,8 +1,10 @@
 import * as basicLightbox from 'basiclightbox';
 import { v4 as uuidv4 } from 'uuid';
 import '../node_modules/basiclightbox/dist/basicLightbox.min.css';
+
 const addRealtyBtn = document.querySelector('.js-add-realty');
 const LS_KEY = 'realty-items';
+
 const realtyItems = JSON.parse(localStorage.getItem(LS_KEY)) ?? [];
 const list = document.querySelector('.js-list');
 let img = null;
@@ -80,68 +82,20 @@ function cardHandler(evt) {
   <h3>Кількість кімнат: ${rooms}</h3>
   <h3>Тип: ${type}</h3>
   <h3>Статус: ${status}</h3>
-  <button class="js-edit">Редагувати</button>
+  <button class="js-realty-info">Управління об'єктом</button>
   </div>`);
   instance.show();
 
-  const editBtn = document.querySelector('.js-edit');
-  editBtn.addEventListener('click', handlerEditMode.bind({ id }));
+  const infoBtn = document.querySelector('.js-realty-info');
+  infoBtn.addEventListener('click', handlerRedirectMode.bind({ id }));
 }
 
 function onLoad() {
   renderImage(this.files[0]);
 }
 
-function handlerEditMode() {
-  const { id } = this;
-  const { title, photo, details, price, area, rooms, type, status } =
-    realtyItems.find(({ id: currentId }) => id === currentId);
-  const instance = basicLightbox.create(`
-  <form action="submit" class="js-form-realty form-realty">
-
-
-  <div class='form-realty__input-block form-realty__input-block_row'>
-    <label for="photo" class='form-realty__label'>Завантажте фото об'єкту</label>
-    <label for="photo" class='form-realty__image-btn'></label>
-    <input type="file" name="realty-photo" id="photo" accept="image/png, image/jpeg" hidden>
-  </div>
-  <div class="js-form-realty__preview"></div>
-
-  <div class='form-realty__input-block'>
-  <label for="text" class='form-realty__label'>Назва вашого об'єкта</label>
-  <input type="text" class='form-realty__input' name="realty-title" id="title">
-  </div>
-
-  <div class='form-realty__input-block'>
-    <label for="details" class='form-realty__label'>Опис об'єкту</label>
-    <textarea type="text" class='form-realty__input' name="realty-details" id="details"></textarea>
-  </div>
-
-  <div class='form-realty__input-block'>
-    <label for="rooms" class='form-realty__label'>Кількість кімнат</label>
-    <input type="number" class='form-realty__input' min="0" name="realty-rooms" id="rooms">
-  </div>
-
-  <div class='form-realty__input-block'>
-    <label for="area" class='form-realty__label'>Площа об'єкту м<sup>2</sup>;</label>
-    <input type="number" class='form-realty__input' min="0" name="realty-area" id="area">
-  </div>
-
-  <div class='form-realty__input-block'>
-    <label for="price" class='form-realty__label'>Вартість об'єкту</label>
-    <input type="number" class='form-realty__input' min="0" name="realty-price" id="price">
-  </div>
-
-  <div class='form-realty__input-block'>
-    <label for="type" class='form-realty__label'>Тип пропозиції</label>
-    <select name="realty-type" id="type" class='form-realty__select'>
-      <option value="Продаж">Продаж</option>
-      <option value="Оренда">Оренда</option>
-    </select>
-  </div>
-  <button class='form-realty__create'>Створити об'єкт</button>
-</form>`);
-  instance.show();
+function handlerRedirectMode() {
+  window.location.href = './manageItem.html?id=' + this.id;
 }
 
 function renderImage(file) {
@@ -161,18 +115,19 @@ function renderImage(file) {
 
 function addRealty(evt) {
   evt.preventDefault();
-  const { details, rooms, area, price, type, title } = evt.currentTarget.elements;
+  const { details, rooms, area, price, type, title } =
+    evt.currentTarget.elements;
 
   const data = {
     id: uuidv4(),
     photo: img,
-    title:title.value,
+    title: title.value,
     details: details.value,
     rooms: rooms.value,
     area: area.value,
     price: price.value,
     type: type.value,
-    status: type.value === 'Продаж' ? 'В продажі' : 'Вільно'
+    status: type.value === 'Продаж' ? 'В продажі' : 'Вільно',
   };
   realtyItems.push(data);
   localStorage.setItem(LS_KEY, JSON.stringify(realtyItems));
